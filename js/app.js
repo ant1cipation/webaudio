53416a21582d955a479b7f7fd9819a4920eb1669
Uni <janssen.malte@gmx.net>
2018-03-18 21:04:48 +0100

    connect visualizer, cleaner css

--- js/app.js
+++ js/app.js
@@ -23,27 +23,27 @@ analyser.fftSize = 256;
 analyser.smoothingTimeConstant = 0.9;
 const bufferLength = analyser.frequencyBinCount;
 const dataArray = new Uint8Array(bufferLength);
-let canvas = qS('.vis-canvas');
+let canvas = qS('.visualizer canvas');
 let canvasCtx = canvas.getContext('2d');
 const width = canvas.width,
 	height = canvas.height;
 canvasCtx.clearRect(0, 0, width, height);
 
 function draw() {
-	const drawVisual = requestAnimationFrame(draw);
+	requestAnimationFrame(draw);
 
 	analyser.getByteFrequencyData(dataArray);
 
 	canvasCtx.fillStyle = '#fff';
 	canvasCtx.fillRect(0, 0, width, height);
 
-	let barWidth = (width / bufferLength) * 2.5;
+	let barWidth = (width / bufferLength);
 	let barHeight, x = 0;
 	for (const e of dataArray) {
 		canvasCtx.fillStyle = '#000';
 		canvasCtx.fillRect(x, height - e, barWidth, e);
 
-		x += barWidth + 1;
+		x += barWidth;
 	}
 }
 draw();
@@ -65,7 +65,7 @@ function getData(path) {
 
 function play() {
 	source = context.createBufferSource();
-	source.connect(context.destination);
+	source.connect(analyser);
 	source.loop = true;
 	source.buffer = buffer;
 	paused = false;
@@ -77,15 +77,19 @@ function play() {
 		startTime = Date.now();
 		source.start(0);
 	}
+
+	qS('.playback button').innerHTML = 'pause';
 };
 
 function pause() {
 	source.stop(0);
 	pauseTime = Date.now() - startTime;
 	paused = true;
+
+	qS('.playback button').innerHTML = 'play_arrow';
 }
 
 // event handlers
-qS('.playbtn').addEventListener('click', () => {
+qS('.playback button').addEventListener('click', () => {
 	paused ? play() : pause();
 });
