449f430e74fb379d66aa07b5feb2902d848e28d1
Michael Herberg <herbergmichael@gmx.de>
2018-03-18 20:15:06 +0100

    js: visualizer

--- js/app.js
+++ js/app.js
@@ -13,7 +13,41 @@ function qSA(q) {
 const context = new AudioContext();
 let source, buffer, paused = true, pauseTime, startTime;
 
+// get source
 getData('sample.wav');
+
+// initialize analyser and visualizer
+const analyser = context.createAnalyser();
+analyser.connect(context.destination);
+analyser.fftSize = 256;
+analyser.smoothingTimeConstant = 0.9;
+const bufferLength = analyser.frequencyBinCount;
+const dataArray = new Uint8Array(bufferLength);
+let canvas = qS('.vis-canvas');
+let canvasCtx = canvas.getContext('2d');
+const width = canvas.width,
+	height = canvas.height;
+canvasCtx.clearRect(0, 0, width, height);
+
+function draw() {
+	const drawVisual = requestAnimationFrame(draw);
+
+	analyser.getByteFrequencyData(dataArray);
+
+	canvasCtx.fillStyle = '#fff';
+	canvasCtx.fillRect(0, 0, width, height);
+
+	let barWidth = (width / bufferLength) * 2.5;
+	let barHeight, x = 0;
+	for (const e of dataArray) {
+		canvasCtx.fillStyle = '#000';
+		canvasCtx.fillRect(x, height - e, barWidth, e);
+
+		x += barWidth + 1;
+	}
+}
+draw();
+
 
 function getData(path) {
 	let request = new XMLHttpRequest();
