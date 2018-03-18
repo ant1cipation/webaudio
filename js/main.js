12f133caac5408c0e8d3b6263354199cd1442512
Michael Herberg <herbergmichael@gmx.de>
2018-03-18 19:30:40 +0100

    js: now decoding audio data

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
 const context = new AudioContext();
+let source;
+
+const getData = path => {
+	source = context.createBufferSource();
+	let request = new XMLHttpRequest();
+	request.open('GET', path, true);
+	request.responseType = 'arraybuffer';
+
+	request.onload = () => {
+		let audioData = request.response;
+
+		context.decodeAudioData(audioData, buffer => {
+			source.buffer = buffer;
+			source.connect(context.destination);
+			source.loop = true;
+		});
+	}
+	request.send();
+}
+
+getData('sample.wav');
