c8a9be7db9a1bb7aef6ec4b87ec8f8ac1de6166b
Uni <janssen.malte@gmx.net>
2018-03-18 20:13:02 +0100

    play/pause using buffer offsets

--- js/app.js
+++ js/app.js
@@ -11,32 +11,47 @@ function qSA(q) {
 
 // code
 const context = new AudioContext();
-let source;
+let source, buffer, paused = true, pauseTime, startTime;
 
 getData('sample.wav');
 
 function getData(path) {
-	source = context.createBufferSource();
 	let request = new XMLHttpRequest();
 	request.open('GET', path, true);
 	request.responseType = 'arraybuffer';
 
 	request.onload = () => {
 		let audioData = request.response;
-
-		context.decodeAudioData(audioData, buffer => {
-			source.buffer = buffer;
-			source.connect(context.destination);
-			source.loop = true;
+		context.decodeAudioData(audioData, b => {
+			buffer = b;
 		});
 	};
 	request.send();
 }
 
+function play() {
+	source = context.createBufferSource();
+	source.connect(context.destination);
+	source.loop = true;
+	source.buffer = buffer;
+	paused = false;
 
-// playback with source.start(0) and source.stop()
+	if (pauseTime) {
+		startTime = Date.now() - pauseTime;
+		source.start(0, pauseTime / 1000);
+	} else {
+		startTime = Date.now();
+		source.start(0);
+	}
+};
+
+function pause() {
+	source.stop(0);
+	pauseTime = Date.now() - startTime;
+	paused = true;
+}
 
 // event handlers
-document.querySelector('.playbtn').addEventListener('click', () => {
-	source.start(0);
-})
+qS('.playbtn').addEventListener('click', () => {
+	paused ? play() : pause();
+});
